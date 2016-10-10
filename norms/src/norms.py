f1839792bb476a67dc1a6cdc5b65427b0f228d1f
i-s-o-g-r-a-m <i-s-o-g-r-a-m@users.noreply.github.com>
2016-10-10 13:03:16 -0400

    Finally some clean-up

--- norms/src/norms.py
+++ norms/src/norms.py
@@ -1,60 +1,46 @@
 from mako.template import Template
 
 
-# TODO add back the text mode stuff
-
-
-# all known point sizes, and the ones that determine the x-axis
-x_axis_sizes = (
+# all known point sizes; the ones that determine the x-axis
+all_point_sizes = (
     5, 6, 7, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 24, 27, 28, 30, 32,
     36, 40, 42, 48, 54, 56, 60, 72, 84, 90, 96, 108, 120, 126, 144, 168
 )
 
-# the common (there is a better term) sizes, and the ones we use for the y-axis
+# the commonly-used (there is a better term) sizes; the ones used on the y-axis
 common_point_sizes = (5, 6, 7, 8, 9, 10, 12, 14, 18, 24, 30, 36, 42, 48, 60, 72)
 
-# point size for the final row
+# the max point size on the y-axis
 y_axis_max = 42
 
-# a 4-em quad is the largest piece of spacing material we have here
+# a 4-em quad is the largest piece of spacing material we would have
 max_quad_size = 4
 
 
-def make_norms():
-
-    # first pass to build the table of values
+def main():
     matrix = []
-    for ptsize in [x for x in common_point_sizes if x <= y_axis_max]:
-        row = []
-        for c in x_axis_sizes:
-            cell_value = None
-            if len([i for i in row if i is not None]) < max_quad_size:
-                if ptsize == c or (c % ptsize == 0):
-                    cell_value = c
-            row.append(cell_value)
-        matrix.append(row)
+    rows = enumerate([x for x in common_point_sizes if x <= y_axis_max])
 
-    # second pass to assemble what we need for rendering in the template
-    output_rows = []
-    for row_idx, row in enumerate(matrix):
-        cells = []
-        for td_idx, td in enumerate(row):
-            cell = {}
-            if td:
-                column_pt_size = x_axis_sizes[td_idx]
-                multiple_use_size = (
-                    column_pt_size != common_point_sizes[row_idx] and
-                    column_pt_size in common_point_sizes
-                )
+    for row_idx, y_size in rows:
+        row = []
+
+        for x_size in all_point_sizes:
+            display_quad = x_size == y_size or (x_size % y_size == 0)
+            quads_displayed = len([x for x in row if x])
+            if display_quad and quads_displayed < max_quad_size:
                 cell = {
-                    'multiple': multiple_use_size,
-                    'width': column_pt_size,
+                    'multiple': x_size != y_size and x_size in common_point_sizes,
+                    'width': str(x_size),
                     'height':  str(common_point_sizes[row_idx])
                 }
-            cells.append(cell)
-        output_rows.append(cells)
+            else:
+                cell = None
+            row.append(cell)
 
-    print(Template(filename="template.html").render(rows=output_rows))
+        matrix.append(row)
+
+    print(Template(filename="template.html").render(rows=matrix))
+
 
 if __name__ == "__main__":
-    make_norms()
+    main()
